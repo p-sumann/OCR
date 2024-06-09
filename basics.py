176fed72258e341accdbd611a55f0c957dad40df
Suman Paudel <dastonsuman1997@gmail.com>
2024-06-09 13:23:36 +0545

    some changes

---
+++ basics.py
@@ -0,0 +1,11 @@
+import cv2
+from PIL import Image
+import pytesseract
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+
+
+image = Image.open('images/Falko PRD - Sales Invoice NAC8_23_0231.pdf_1.png')
+print(im)
